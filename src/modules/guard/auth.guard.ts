c10dd0caf0d63a0a3e55af94a181c1fff4175344
kai <34413109+kimtony@users.noreply.github.com>
2024-06-07 19:50:31 +0800

    Update auth.guard.ts

--- src/modules/guard/auth.guard.ts
+++ src/modules/guard/auth.guard.ts
@@ -19,27 +19,29 @@ import {
   export class AuthGuard implements CanActivate {
     constructor(private readonly redisService: RedisService) {}
     async canActivate(context: ExecutionContext): Promise<boolean> {
-        // const request = context.switchToHttp().getRequest();
-        // const token = context.switchToRpc().getData().headers.token || context.switchToHttp().getRequest().body.token || getUrlQuery(request.url, 'token');
-        // const pass = isExistInArr(apiNotAuthArr,request.url);
-        // if (pass) {
-        //     return true;
-        // }else{
-        //     // 需要token验证
-        //     if (!token) {
-        //         // 如果传递了token的话就要从redis中查询是否有该token
-        //         const result = await this.redisService.get(token);
-        //         if (result) {
-        //             // 这里我们知道result数据类型就是我们定义的直接断言
-        //             request.user = result;
-        //             return true;
-        //         } else {
-        //             throw new HttpException( { message: '你还没登录,请先登录' },HttpStatus.UNAUTHORIZED );
-        //         }
-        //     }else{
-        //         throw new HttpException( { message: '你还没登录,请先登录' },HttpStatus.UNAUTHORIZED );
-        //     }
-        // }
+        const request = context.switchToHttp().getRequest();
+        let token;
+        if (context.switchToRpc().getData().headers || context.switchToHttp().getRequest().body) {
+            token = context.switchToRpc().getData().headers.token || context.switchToHttp().getRequest().body.token || getUrlQuery(request.url, 'token');
+        }
+        const pass = isExistInArr(apiNotAuthArr,request.url);
+        if (pass) {
+            return true;
+        }
+        // 需要token验证
+        if (!token) {
+            throw new HttpException( { message: '你还没登录,请先登录' },HttpStatus.UNAUTHORIZED );
+        }
+
+        // 如果传递了token的话就要从redis中查询是否有该token
+        const result = await this.redisService.get(token);
+        if (!result) {
+            // 如果 Redis 中没有对应的用户信息，则抛出异常
+            throw new HttpException({ message: '你还没登录，请先登录' }, HttpStatus.UNAUTHORIZED);
+        }
+
+        // 将用户信息存储到请求中，方便后续使用
+        request.user = result;
         return true;
 
     }
